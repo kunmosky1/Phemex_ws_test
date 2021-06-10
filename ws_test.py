79b6b52c36b3c1ba2429c6944a61f2e65234cc7d
kunmosky1 <kiyoshi@gumbo-star.sakura.ne.jp>
2021-06-10 21:15:16 +0900

    Support heatbeat

--- ws_test.py
+++ ws_test.py
@@ -12,6 +12,18 @@ class WebsocketConnection(object):
         self._endpoint = "wss://phemex.com/ws"
         self._start()
 
+    def _ping_thread(self, ws):
+        last_ping_time = time.time()
+        while True:
+            if time.time() - last_ping_time >= 30:
+                last_ping_time = time.time()
+                try:
+                    ws.send('')
+                    print('Phemex send ping.')
+                except Exception as e:
+                    print('Phemex ping error!')
+            time.sleep(5)
+
     def _start(self):
         def _on_open(ws):
             print( "websocket connected to '" + self._endpoint + "'" )
@@ -41,10 +53,14 @@ class WebsocketConnection(object):
                 print( msg )
 
         self.ws = websocket.WebSocketApp( self._endpoint, on_open=_on_open, on_message=_on_message, on_error=_on_error, on_close=_on_close )
-        self._thread = Thread(target=_run, args=(self.ws, ))
-        self._thread.daemon = True
-        self._thread.start()
+        thread1 = Thread(target=_run, args=(self.ws, ))
+        thread1.daemon = True
+        thread1.start()
 
+        # ping
+        thread2 = Thread(target=self._ping_thread, args=(self.ws, ))
+        thread2.daemon = True
+        thread2.start()
 
 if __name__ == '__main__':
 
